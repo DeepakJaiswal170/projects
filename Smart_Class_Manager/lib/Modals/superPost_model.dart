cae5175f430e8f6360df47797ee8cd9a838045c2
DeepakJaiswal170 <99420070+DeepakJaiswal170@users.noreply.github.com>
2022-03-05 21:20:49 +0530

    Delete Smart_Class_Manager/lib directory

--- Smart_Class_Manager/lib/Modals/superPost_model.dart
+++
@@ -1,7 +0,0 @@
-import 'package:api_check/Modals/post_model.dart';
-
-class SuperModel {
-  List<Post> data;
-  SuperModel({this.data});
-  SuperModel.fromJson(Map<String, dynamic> json) : data = (json['data'] as List)?.map((e) => e == null ? null :Post.fromJson(e as Map<String, dynamic>))?.toList();
-}
